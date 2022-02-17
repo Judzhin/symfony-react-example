0c03f8c19a5b11021e3133cc6ca0ee7a6b2ba8e2
Judzhin Miles <info@msbios.com>
2022-02-17 11:50:11 +0200

    Include DB && Create Migration

---
+++ src/Entity/Task.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: TaskRepository::class)]
+#[ORM\Table(name: 'todos')]
+class Task
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private ?int $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    #[ORM\Column(type: 'string', length: 200)]
+    private string $name;
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Task
+     */
+    public function setName(string $name): Task
+    {
+        $this->name = $name;
+        return $this;
+    }
+}
